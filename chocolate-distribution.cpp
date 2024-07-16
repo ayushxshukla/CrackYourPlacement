f3d1a4aa1507931a714a0f4a3063b13bef6ca62e
Ayush Shukla <ayush.01344@gmail.com>
2024-07-16 02:28:26 +0000

    one

---
+++ chocolate-distribution.cpp
@@ -0,0 +1,11 @@
+ public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+        sort(a.begin(),a.end());
+        long long ans=INT_MAX;
+        for(int i=m-1;i<n;i++){
+            ans=min(ans,a[i]-a[i-(m-1)]);
+        }
+        
+        return ans;
+    
+    }   
